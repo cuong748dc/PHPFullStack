043b54e9afd84d494faeb0534f84f49252657e78
Cuong <cuong748dc@gmail.com>
2020-01-09 17:25:09 +0700

    sua bai buoi 3

--- Buoi 3/Bai3.php
+++ Buoi 3/Bai3.php
@@ -6,10 +6,4 @@ function printStarts()
     }
 }
 
-$n = "hoc php full stack";
-printStarts();
-echo "\n";
-echo "Chuoi da cho co " . str_word_count() . " tu";
-echo "\n";
-printStarts();
 ?>
--- Buoi 3/Bai4.php
+++ Buoi 3/Bai4.php
@@ -1,8 +1,22 @@
 <?php
+
 function printStarts()
 {
     for ($i = 0; $i < 20; $i++) {
         echo "*";
     }
 }
+
+$s1 = "123";
+$s2 = "1234";
+
+printStarts();
+echo "\n";
+if (strpos($s1, $s2) == true) {
+    echo "co";
+} else {
+    echo "khong";
+}
+echo "\n";
+printStarts();
 ?>
